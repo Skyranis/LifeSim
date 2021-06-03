88261c9c32ffd80ea3128b4745afbcdd48222d1e
Skyranis <85298849+Skyranis@users.noreply.github.com>
2021-06-03 11:05:38 -0400

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -55,61 +55,30 @@ xui.Class('App', 'xui.Module',{
             );
             
             host.xui_ui_tabs4.append(
-                xui.create("xui.UI.Layout")
-                .setHost(host,"xui_ui_layout17")
+                xui.create("xui.UI.ToolBar")
+                .setHost(host,"xui_ui_toolbar14")
                 .setItems([
                     {
-                        "id":"before",
-                        "size":55,
-                        "min":10,
-                        "locked":false,
-                        "folded":false,
-                        "hidden":false,
-                        "cmd":false,
-                        "html":"",
-                        "pos":"before"
-                    },
-                    {
-                        "id":"main",
-                        "size":80,
-                        "min":10
-                    },
-                    {
-                        "id":"after",
-                        "size":80,
-                        "min":10,
-                        "locked":false,
-                        "folded":false,
-                        "hidden":false,
-                        "cmd":false,
-                        "pos":"after"
+                        "id":"grp1",
+                        "sub":[
+                            {
+                                "id":"a1",
+                                "caption":"button"
+                            },
+                            {
+                                "id":"a2",
+                                "type":"split"
+                            },
+                            {
+                                "id":"a3",
+                                "caption":"button3"
+                            }
+                        ],
+                        "caption":"grp1"
                     }
                 ])
-                .setAutoTips(false)
-                .setDisableTips(true)
-                .setLeft("0px")
-                .setTop("0px")
-                .setReadonly(true),
+                .setTop("160px"),
                 "a"
-            );
-            
-            host.xui_ui_layout17.append(
-                xui.create("xui.UI.Label")
-                .setHost(host,"xui_ui_label22")
-                .setLeft("9px")
-                .setTop("5px")
-                .setWidth("172px")
-                .setHeight("28px")
-                .setBottom("0px")
-                .setZIndex(3)
-                .setTabindex(2)
-                .setDisplay("inline")
-                .setCaption("Sim Name")
-                .setHAlign("left")
-                .setFontColor("#6495ED")
-                .setFontSize("22px")
-                .setFontWeight("bold"),
-                "before"
             );
             
             return children;
@@ -120,6 +89,8 @@ xui.Class('App', 'xui.Module',{
         customAppend : function(parent, subId, left, top){
             // "return false" will cause all the internal UI controls will be added to the parent panel
             return false;
+        },
+        functions:{
         }
         /*,
         // To determine how properties affects this module
